156a4db388bff89f37daddf24569dc417dd0cb05
ImperialOfficer <payne.kenneth.54321@gmail.com>
2026-01-07 14:05:56 -0600

    started pokedex

---
+++ PokeDexViewer/app/src/main/java/com/example/pokedexviewer/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.pokedexviewer;
+
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.GestureDetectorCompat;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+    }
+}
