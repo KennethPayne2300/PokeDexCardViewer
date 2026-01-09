c656b28fe2a1a6438e26048d56e153a4c12da8a3
ImperialOfficer <payne.kenneth.54321@gmail.com>
2026-01-09 14:06:33 -0600

    finished card navigation and card making

--- PokeDexViewer/app/src/main/java/com/example/pokedexviewer/MainActivity.java
+++ PokeDexViewer/app/src/main/java/com/example/pokedexviewer/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.pokedexviewer;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -18,6 +20,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView pokemonStageImg, pokemonImg, pokemonTypeImg;
+    private TextView pokemonNameText,pokemonStageText,pokemonHPText,pokemonDescriptionText,pokemonLengthText,pokemonWeightText,pokemonMove1Text,pokemonMove1PowerText,pokemonMove2Text,pokemonMove2PowerText,pokemonWeaknessText,pokemonResistanceText;
+    private LinearLayout retreatLayout;
+    private Button backBTN,nextBTN;
+    int index;
+    private Pokemon[] pokemons;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,33 +34,105 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //get image views
-        ImageView pokemonStageImg = findViewById(R.id.pokemonStageImageView);
-        ImageView pokemonImg = findViewById(R.id.pokemonImageView);
-        ImageView pokemonTypeImg = findViewById(R.id.pokemonTypeImageView);
+        pokemonStageImg = findViewById(R.id.pokemonStageImageView);
+        pokemonImg = findViewById(R.id.pokemonImageView);
+        pokemonTypeImg = findViewById(R.id.pokemonTypeImageView);
         //get text views
-        TextView pokemonNameText = findViewById(R.id.pokemonNameTextView);
-        TextView pokemonStageText = findViewById(R.id.pokemonStageTextView);
-        TextView pokemonHPText = findViewById(R.id.pokemonHPTextView);
-        TextView pokemonDescriptionText = findViewById(R.id.pokemonDescriptionTextView);
-        TextView pokemonLengthText = findViewById(R.id.pokemonLengthTextView);
-        TextView pokemonWeightText = findViewById(R.id.pokemonWeightTextView);
-        TextView pokemonMove1Text = findViewById(R.id.pokemonMove1TextView);
-        TextView pokemonMove1PowerText = findViewById(R.id.pokemonMove1PowerTextView);
-        TextView pokemonMove2Text = findViewById(R.id.pokemonMove2TextView);
-        TextView pokemonMove2PowerText = findViewById(R.id.pokemonMove2PowerTextView);
-        TextView pokemonWeaknessText = findViewById(R.id.pokemonWeaknessTextView);
-        TextView pokemonResistanceText = findViewById(R.id.pokemonResistanceTextView);
+        pokemonNameText = findViewById(R.id.pokemonNameTextView);
+        pokemonStageText = findViewById(R.id.pokemonStageTextView);
+        pokemonHPText = findViewById(R.id.pokemonHPTextView);
+        pokemonDescriptionText = findViewById(R.id.pokemonDescriptionTextView);
+        pokemonLengthText = findViewById(R.id.pokemonLengthTextView);
+        pokemonWeightText = findViewById(R.id.pokemonWeightTextView);
+        pokemonMove1Text = findViewById(R.id.pokemonMove1TextView);
+        pokemonMove1PowerText = findViewById(R.id.pokemonMove1PowerTextView);
+        pokemonMove2Text = findViewById(R.id.pokemonMove2TextView);
+        pokemonMove2PowerText = findViewById(R.id.pokemonMove2PowerTextView);
+        pokemonWeaknessText = findViewById(R.id.pokemonWeaknessTextView);
+        pokemonResistanceText = findViewById(R.id.pokemonResistanceTextView);
         //get the retreat layout
-        LinearLayout retreatLayout = findViewById(R.id.retreatCostLayout);
+        retreatLayout = findViewById(R.id.retreatCostLayout);
+        //get buttons
+        backBTN = findViewById(R.id.previousCardBTN);
+        nextBTN = findViewById(R.id.nextCardBTN);
 
-        Pokemon[] pokemons = new Pokemon[3];
+        index = 0;
 
+        pokemons = new Pokemon[3];
+        //add pokemons to the pokemon list
         pokemons[0]=new Pokemon("pikachu", "growl", "pika bolt", "fighting", "none", "When it is angered, it immediately discharges the energy stored in the pouches in its cheeks.", "Length: 0.4m", 13.2, 20, 30,1,0,70,R.drawable.pickachu,R.drawable.electrictype);
-        //pokemons[1]=new Pokemon("Charizard", "Energy Burn", "Fire Spin", "Water", "Fighting", "If Charizard becomes truly angered, the flame at the tip of its tail burns in a light blue shade.","Height: 5'07\"", 199.5);
-        try {
-            pokemons[0].makeCard(pokemonImg, pokemonNameText, pokemonStageImg, pokemonStageText, pokemonHPText, pokemonTypeImg, pokemonDescriptionText, pokemonLengthText, pokemonWeightText, pokemonMove1Text, pokemonMove1PowerText, pokemonMove2Text, pokemonMove2PowerText, pokemonWeaknessText, pokemonResistanceText, retreatLayout, this);
-        } catch(Exception e){
-            Log.d("make card", e.toString());
+        pokemons[1]=new Pokemon("Charizard", "Brave Wing", "Explosive Vortex", "Water", "none", "If Charizard becomes truly angered, the flame at the tip of its tail burns in a light blue shade.","Height: 5'07\"", 199.5,60,330,2,2,330,R.drawable.charizard,R.drawable.babycharizard,R.drawable.firetype);
+        pokemons[2]=new Pokemon("Squrtle","Tackle","Rain Splash","grass","none","After birth, its back swells and hardens into a shell. It sprays a potent foam from its mouth","Height: 1'08\"",19.8,10,20,1,0,70,R.drawable.squirtle,R.drawable.watertype);
+        //start with the first pokemon in the list
+        makeCard(pokemons[0]);
+
+        backBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //reset retreat cost images
+                retreatLayout.removeAllViews();
+                //change to the previous pokemon and if its at the start of the list go to the last pokemon
+                if(index>0){
+                    index--;
+                } else{
+                    index=pokemons.length-1;
+                }
+                //make the card
+                makeCard(pokemons[index]);
+            }
+        });
+        nextBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //reset retreat cost images
+                retreatLayout.removeAllViews();
+                //change to the next pokemon and if its at end of the list go to the first pokemon in it.
+                if(index<pokemons.length-1){
+                    index++;
+                } else{
+                    index=0;
+                }
+                try {
+                    makeCard(pokemons[index]);
+                } catch (Exception e){
+                    Log.d("make card", e.toString());
+                }
+            }
+        });
+    }
+    public void makeCard(Pokemon pokemon){
+        pokemonNameText.setText(pokemon.getName());
+        pokemonImg.setImageResource(pokemon.getPokemonImg());
+        Log.d("pokemon image",pokemon.getTypeImg()+" "+pokemon.getName());
+        if(pokemon.getStageImg() != 0){
+            pokemonStageImg.setImageResource(pokemon.getStageImg());
+            pokemonStageImg.setVisibility(View.VISIBLE);
+        } else {
+            pokemonStageImg.setVisibility(View.GONE);
+        }
+        pokemonStageText.setText(String.format("Stage %d", pokemon.getStage()));
+        pokemonHPText.setText(String.format("%d hp",pokemon.getHealth()));
+        pokemonTypeImg.setImageResource(pokemon.getTypeImg());
+        pokemonDescriptionText.setText(pokemon.getDescription());
+        pokemonLengthText.setText(pokemon.getLength());
+        pokemonWeightText.setText(String.format("weight: %.1f lbs",pokemon.getWeight()));
+        pokemonMove1Text.setText(pokemon.getMove1());
+        pokemonMove1PowerText.setText(String.format("%d",pokemon.getMove1dmg()));
+        pokemonMove2Text.setText(pokemon.getMove2());
+        pokemonMove2PowerText.setText(String.format("%d",pokemon.getMove2dmg()));
+        pokemonWeaknessText.setText(pokemon.getWeakness());
+        pokemonResistanceText.setText(pokemon.getResistance());
+
+        //repeat for how much retreat costs, add an image per retreat cost.
+        for(int i=0; i<pokemon.getRetreatCost(); i++){
+            //make the image view
+            ImageView retreatSymbol = new ImageView(this);
+            //set the image
+            retreatSymbol.setImageResource(R.drawable.retreat_symbol);
+            //set the layout parameters
+            retreatSymbol.setLayoutParams(new LinearLayout.LayoutParams(50,50));
+            //add to layout
+            retreatLayout.addView(retreatSymbol);
         }
     }
 }
--- PokeDexViewer/app/src/main/java/com/example/pokedexviewer/Pokemon.java
+++ PokeDexViewer/app/src/main/java/com/example/pokedexviewer/Pokemon.java
@@ -1,18 +1,8 @@
 package com.example.pokedexviewer;
-
-import android.app.ActionBar;
-import android.content.Context;
-import android.graphics.drawable.Drawable;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
 public class Pokemon {
-    private String name, move1, move2, weakness, resistance, length, description;
-    private Double weight;
-    private int move1dmg, move2dmg, retreatCost, stage, health, pokemonImg, stageImg, typeImg;
+    private final String name, move1, move2, weakness, resistance, length, description;
+    private final Double weight;
+    private final int move1dmg, move2dmg, retreatCost, stage, health, pokemonImg, stageImg, typeImg;
 
     public Pokemon(String name, String move1, String move2, String weakness, String resistance, String description, String length, Double weight, int move1dmg, int move2dmg, int retreatCost, int stage,int health, int pokemonImg, int stageImg, int typeImg){
         this.name=name;
@@ -48,40 +38,71 @@ public class Pokemon {
         this.stage=stage;
         this.health=health;
         this.pokemonImg=pokemonImg;
+        this.stageImg=0;
         this.typeImg=typeImg;
     }
 
-    public void makeCard(ImageView pokemonIMG, TextView nameText, ImageView pokemonStageImage, TextView stageText, TextView hpText,ImageView typeImgView, TextView descriptionText, TextView lengthText, TextView weightText, TextView move1Text, TextView move1dmgText, TextView move2Text, TextView move2dmgText, TextView weaknessText, TextView resistanceText, LinearLayout retreatCostLayout, Context context){
-        nameText.setText(name);
-        pokemonIMG.setImageResource(pokemonImg);
-        if(stageImg != 0){
-            pokemonStageImage.setImageResource(stageImg);
-        } else {
-            pokemonStageImage.setVisibility(View.GONE);
-        }
-        stageText.setText(String.format("Stage %d", stage));
-        hpText.setText(String.format("%d hp",health));
-        typeImgView.setImageResource(typeImg);
-        descriptionText.setText(description);
-        lengthText.setText(length);
-        weightText.setText(String.format("weight: %.1f lbs",weight));
-        move1Text.setText(move1);
-        move1dmgText.setText(move1dmg);
-        move2Text.setText(move2);
-        move2dmgText.setText(move2dmg);
-        weaknessText.setText(weakness);
-        resistanceText.setText(resistance);
+    public String getName() {
+        return name;
+    }
 
-        //repeat for how much retreat costs, add an image per retreat cost.
-        for(int i=0; i<retreatCost; i++){
-            //make the image view
-            ImageView retreatSymbol = new ImageView(context);
-            //set the image
-            retreatSymbol.setImageResource(R.drawable.retreat_cost_symbol);
-            //set the layout parameters
-            retreatSymbol.setLayoutParams(new LinearLayout.LayoutParams(10,10));
-            //add to layout
-            retreatCostLayout.addView(retreatSymbol);
-        }
+    public String getMove1() {
+        return move1;
+    }
+
+    public String getMove2() {
+        return move2;
+    }
+
+    public String getWeakness() {
+        return weakness;
+    }
+
+    public String getResistance() {
+        return resistance;
+    }
+
+    public String getLength() {
+        return length;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public int getMove1dmg() {
+        return move1dmg;
+    }
+
+    public int getMove2dmg() {
+        return move2dmg;
+    }
+
+    public int getRetreatCost() {
+        return retreatCost;
+    }
+
+    public int getStage() {
+        return stage;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getPokemonImg() {
+        return pokemonImg;
+    }
+
+    public int getStageImg() {
+        return stageImg;
+    }
+
+    public int getTypeImg() {
+        return typeImg;
     }
 }
