585c71ebe6c012433ac1fde42520fe062f46d552
KennethPayne2300 <kennethpayne353@gmail.com>
2026-01-10 12:34:25 -0600

    finished pokedex

--- PokeDexViewer/app/src/main/java/com/example/pokedexviewer/MainActivity.java
+++ PokeDexViewer/app/src/main/java/com/example/pokedexviewer/MainActivity.java
@@ -1,31 +1,28 @@
 package com.example.pokedexviewer;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.GestureDetectorCompat;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView pokemonStageImg, pokemonImg, pokemonTypeImg;
     private TextView pokemonNameText,pokemonStageText,pokemonHPText,pokemonDescriptionText,pokemonLengthText,pokemonWeightText,pokemonMove1Text,pokemonMove1PowerText,pokemonMove2Text,pokemonMove2PowerText,pokemonWeaknessText,pokemonResistanceText;
     private LinearLayout retreatLayout;
-    private Button backBTN,nextBTN;
+    private Spinner pokeSpinner;
     int index;
-    private Pokemon[] pokemons;
+    private ArrayList<Pokemon> pokemons;
+    private ArrayList<String> pokemonNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,58 +49,51 @@ public class MainActivity extends AppCompatActivity {
         pokemonResistanceText = findViewById(R.id.pokemonResistanceTextView);
         //get the retreat layout
         retreatLayout = findViewById(R.id.retreatCostLayout);
-        //get buttons
-        backBTN = findViewById(R.id.previousCardBTN);
-        nextBTN = findViewById(R.id.nextCardBTN);
+        //get the Spinner
+        pokeSpinner = findViewById(R.id.PokeSpinner);
 
         index = 0;
 
-        pokemons = new Pokemon[3];
+        pokemons = new ArrayList<>();
         //add pokemons to the pokemon list
-        pokemons[0]=new Pokemon("pikachu", "growl", "pika bolt", "fighting", "none", "When it is angered, it immediately discharges the energy stored in the pouches in its cheeks.", "Length: 0.4m", 13.2, 20, 30,1,0,70,R.drawable.pickachu,R.drawable.electrictype);
-        pokemons[1]=new Pokemon("Charizard", "Brave Wing", "Explosive Vortex", "Water", "none", "If Charizard becomes truly angered, the flame at the tip of its tail burns in a light blue shade.","Height: 5'07\"", 199.5,60,330,2,2,330,R.drawable.charizard,R.drawable.babycharizard,R.drawable.firetype);
-        pokemons[2]=new Pokemon("Squrtle","Tackle","Rain Splash","grass","none","After birth, its back swells and hardens into a shell. It sprays a potent foam from its mouth","Height: 1'08\"",19.8,10,20,1,0,70,R.drawable.squirtle,R.drawable.watertype);
+        pokemons.add(new Pokemon("pikachu", "growl", "pika bolt", "fighting", "none", "When it is angered, it immediately discharges the energy stored in the pouches in its cheeks.", "Length: 0.4m", 13.2, 20, 30,1,0,70,R.drawable.pickachu,R.drawable.electrictype));
+        pokemons.add(new Pokemon("Charizard", "Brave Wing", "Explosive Vortex", "Water", "none", "If Charizard becomes truly angered, the flame at the tip of its tail burns in a light blue shade.","Height: 5'07\"", 199.5,60,330,2,2,330,R.drawable.charizard,R.drawable.babycharizard,R.drawable.firetype));
+        pokemons.add(new Pokemon("Squrtle","Tackle","Rain Splash","grass","none","After birth, its back swells and hardens into a shell. It sprays a potent foam from its mouth","Height: 1'08\"",19.8,10,20,1,0,70,R.drawable.squirtle,R.drawable.watertype));
         //start with the first pokemon in the list
-        makeCard(pokemons[0]);
+        makeCard(pokemons.get(0));
+        //separate array list so that we can see the pokemon names
+        pokemonNames = new ArrayList<>();
+        //add pokemon names to pokemonNames list
+        pokemonNames.add(pokemons.get(0).getName());
+        pokemonNames.add(pokemons.get(1).getName());
+        pokemonNames.add(pokemons.get(2).getName());
 
-        backBTN.setOnClickListener(new View.OnClickListener() {
+        pokeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                //reset retreat cost images
-                retreatLayout.removeAllViews();
-                //change to the previous pokemon and if its at the start of the list go to the last pokemon
-                if(index>0){
-                    index--;
-                } else{
-                    index=pokemons.length-1;
-                }
-                //make the card
-                makeCard(pokemons[index]);
-            }
-        });
-        nextBTN.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //reset retreat cost images
-                retreatLayout.removeAllViews();
-                //change to the next pokemon and if its at end of the list go to the first pokemon in it.
-                if(index<pokemons.length-1){
-                    index++;
-                } else{
-                    index=0;
-                }
-                try {
-                    makeCard(pokemons[index]);
-                } catch (Exception e){
-                    Log.d("make card", e.toString());
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long id) {
+                String item = adapterView.getItemAtPosition(i).toString();
+                for(Pokemon pokemon:pokemons){
+                    if(item.equals(pokemon.getName())){
+                        retreatLayout.removeAllViews();
+                        makeCard(pokemon);
+                    }
                 }
             }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
         });
+        //array adapter to put the names array, into the spinner so we can choose with it.
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,R.layout.poke_spinner_item,pokemonNames);
+        arrayAdapter.setDropDownViewResource(android.R.layout.select_dialog_singlechoice);
+        pokeSpinner.setAdapter(arrayAdapter);
     }
     public void makeCard(Pokemon pokemon){
+        //set the text and images to the data in the pokemon class
         pokemonNameText.setText(pokemon.getName());
         pokemonImg.setImageResource(pokemon.getPokemonImg());
-        Log.d("pokemon image",pokemon.getTypeImg()+" "+pokemon.getName());
         if(pokemon.getStageImg() != 0){
             pokemonStageImg.setImageResource(pokemon.getStageImg());
             pokemonStageImg.setVisibility(View.VISIBLE);
